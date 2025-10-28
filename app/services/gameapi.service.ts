80bca107893528e99f845bb9d454e9bc7139d570
evan <212317146+Distant-Skyline-Xtal@users.noreply.github.com>
2025-10-28 13:06:05 +0900

    Update API_BASE_URL for prod environment

--- app/services/gameapi.service.ts
+++ app/services/gameapi.service.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import type { genreTag, platformTag, SearchTagType } from '~/types/tagTypes';
 import type { gameResult } from '~/types/resultTypes';
 
-const API_BASE_URL = 'http://localhost:5000';
+const API_BASE_URL = import.meta.env.PROD ? '' : 'http://localhost:5000';
 
 export const apiClient = axios.create({
     baseURL: API_BASE_URL,
